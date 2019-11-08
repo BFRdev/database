09459b6c88f742d87934a51b06d6c39f4f2165ab
Ameer <ameeryasin17@gmail.com>
2019-11-08 13:24:13 -0500

    fixed file, added readME

--- App.js
+++ App.js
@@ -1,36 +1,48 @@
 
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar, ListView } from 'react-native';
-import { Container, Content, Header, Form, Input, Item, Button, Label, Icon, List, ListItem } from 'native-base'
+import { StyleSheet, Text, View, StatusBar, FlatList, SafeAreaView } from 'react-native';
+import { Container, Content, Header, Form, Input, Item, Button, Label, Icon, ListItem } from 'native-base'
 
 import firebase from './firebase'
 
 var data = []
 
+
+
 export default class App extends React.Component {
+
+
 
   constructor(props) {
     super(props);
 
-    this.ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 })
+    // this.ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 })
 
     this.state = {
       listViewData: data,
       newContact: ""
     }
 
-  }
 
+
+  }
+  test = firebase.database().ref('/contacts');
   componentDidMount() {
 
     var that = this
     //add to child 
-    firebase.database().ref('/contacts').on('child_added', function (data) {
+    this.test.on('value', snapshot => {
+      const value = snapshot.val();
 
       var newData = [...that.state.listViewData]
+      console.log(newData)
       //push data to database 
-      newData.push(data)
-      that.setState({ listViewData: newData })
+      // newData.push(data)
+      this.setState({ listViewData: value })
+
+      //convert object into array 
+      const result = Object.keys(value).map((key) => value[key]);
+      this.setState({ result })
 
     })
 
@@ -40,7 +52,7 @@ export default class App extends React.Component {
   addRow(data) {
 
     var key = firebase.database().ref('/contacts').push().key
-    firebase.database().ref('/contacts').child(key).set({ name: data })
+    // firebase.database().ref('/contacts').child().set({ name: data })
   }
 
   //delete data 
@@ -60,10 +72,18 @@ export default class App extends React.Component {
 
   }
 
+
+
+
+
   render() {
     return (
-      <Container style={styles.container}>
-        {/* using native Base to build UI, StatusBar is for android */}
+
+        <SafeAreaView>
+          
+          <Container style={styles.container}>
+          
+          {/* using native Base to build UI, StatusBar is for android */}
         <Header style={{ marginTop: StatusBar.currentHeight }}>
           <Content>
             <Item>
@@ -79,38 +99,19 @@ export default class App extends React.Component {
             </Item>
           </Content>
         </Header>
-        <Content>
-          <List
-            //put data into rows 
-            enableEmptySections
-            dataSource={this.ds.cloneWithRows(this.state.listViewData)}
-            renderRow={data =>
-              <ListItem>
-                {/* display list item  */}
-                <Text> {data.val().name}</Text>
-              </ListItem>
-            }
-            // info btn when swipe left 
-            renderLeftHiddenRow={data =>
-              <Button full onPress={() => this.addRow(data)} >
-                <Icon name="information-circle" />
-              </Button>
-            }
 
-            // right swipe to delete 
-            renderRightHiddenRow={(data, secId, rowId, rowMap) =>
-              <Button full danger onPress={() => this.deleteRow(secId, rowId, rowMap, data)}>
-                <Icon name="trash" />
-              </Button>
-            }
+          <FlatList>
+        {/* display number of data */}
+          <Text style={styles.text}>
+            {this.state.result && this.state.result.length ? this.state.result.length : `nothing ${JSON.stringify(this.state.result)}`}
+          </Text>
 
-            //open btn's at value 
-            leftOpenValue={-75}
-            rightOpenValue={-75}
-          />
+          </FlatList>
+        </Container>
+          
+        </SafeAreaView>
 
-        </Content>
-      </Container>
+
     );
   }
 }
@@ -124,109 +125,9 @@ const styles = StyleSheet.create({
   addBtn: {
     backgroundColor: '#6cc5cc',
   },
+  text: {
+    color: 'black', 
+    fontSize: 30, 
+  },
 });
-
-// import React, { Component } from 'react';
-// import { StyleSheet, Text, View, SafeAreaView, ListView } from 'react-native';
-// import { Container, Content, Header, Form, Input, Item, Button, Label, Icon, List, ListItem } from 'native-base'
-
-// import firebase from './firebase';
-
-// var data = []
-
-// export default class App extends React.Component {
-
-//   constructor(props) {
-//     super(props);
-
-//     //list view data source 
-//     this.ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 })
-
-//     this.state = {
-//       listViewData: data,
-//       newContact: ""
-//     }
-//   }
-
-//   componentDidMount() {
-//     var that = this
-
-//     firebase.database().ref('/toDo').on('child_added', function (data) {
-//       var newData = [...that.state.listViewData]
-//       newData.push(data)
-//       that.setState({ listViewData: newData })
-//     })
-//   }
-
-//   addRow(data) {
-//     var key = firebase.database().ref('/toDo').push.key
-//     firebase.database().ref('/toDo').child(key).set({ name: data })
-//   }
-
-//   async deleteRow(data, secId, rowId, rowMap) {
-//     await firebase.database().ref('toDo' + data).set(null)
-//     rowMap[`${secId} ${rowId}`].props.closeRow();
-//     var newData = [... this.state.listViewData];
-//     newData.splice(rowId, 1);
-//     this.setState({ listViewData: newData });
-//   }
-
-//   showInformation() {
-
-//   }
-
-
-//   render() {
-//     return (
-
-//       <Container style={styles.container}>
-//         <Header>
-//           <Content>
-//             <Item>
-//               <Input onChangeText={(newContact) => this.setState({ newContact })}
-//                 placeholder="Add toDo"
-//               />
-//               <Button onPress={() => this.addRow(this.state.newContact)}>
-//                 <Icon name="add" />
-//               </Button>
-//             </Item>
-//           </Content>
-//         </Header>
-
-//         <Content>
-//           <List enableEmptySections
-//             dataSource={this.ds.cloneWithRows(this.state.listViewData)}
-//             renderRow={data =>
-//               <ListItem>
-//                 <Text> {data.val().name} </Text>
-//               </ListItem>
-//             }
-//             renderLeftHiddenRow={data =>
-//               <Button full onPress={() => this.addRow(data)}>
-//                 <Icon name='information-circle' />
-//               </Button>
-//             }
-//             renderRightHiddenRow={(data, secId, rowId, rowMap) =>
-//               <Button full danger onPress={() => this.deleteRow(secId, rowId, rowMap, data)}>
-//                 <Icon name='trash' />
-//               </Button>
-//             }
-
-//             leftOpenValue={-75}
-//             rightOpenValue={-75}
-
-//           />
-//         </Content>
-//       </Container>
-//     )
-//   }
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-
-//   },
-// });
 
---
+++ readME.md
@@ -0,0 +1,22 @@
+# Assignment 2: React Native 
+
+
+This is an attempt of me using firebase to display data in to the database. I am pushing and getting data. 
+
+## About App
+
+This is a to do list app. Users can input data and display it as a list. It used Firebase realtime database to store the data. 
+I intended to make this app way more saphisticated but I do not beleive I learned the necessary skills to do that. 
+
+## Install dependencies 
+
+After cloning the file, in it's directory:
+
+Install node moduals: 
+````
+npm install
+````
+UI library: 
+````
+npm install native-base 
+````
